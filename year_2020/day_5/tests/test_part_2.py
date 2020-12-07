ae4630441c5eb8edcecb69eea7a8b141f278cac1
Anton Mulder <anton.mulder@wearespindle.com>
2020-12-07 21:48:43 +0100

    Fix test

--- year_2020/day_5/tests/test_part_2.py
+++ year_2020/day_5/tests/test_part_2.py
@@ -5,7 +5,7 @@ from year_2020.day_5.part_2 import run
 
 @pytest.mark.parametrize(
     'test_input, expected',
-    [(['FFFBBBFRLR', 'FFFBBBFRRL', 'FFFFBFFRLR'], 36)],
+    [(['BFFFBBFRRR', 'FFFBBBFRLR', 'FFFBBBFRRL', 'FFFFBFFRLR', 'FFFFBFFLRR'], 36)],
 )
 def test_run(test_input, expected):
     assert run(test_input) == expected
