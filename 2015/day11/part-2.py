6d8aaec9d954f1717111064e7c436a5f1b609e60
Anton Mulder <anton.mulder@wearespindle.com>
2025-10-22 22:51:15 +0200

    2015 - day 11

---
+++ 2015/day11/part-2.py
@@ -0,0 +1,27 @@
+import re
+
+from utils.io import read_example
+
+password = read_example()
+
+
+def next_password(password: str, index=7) -> str:
+    if password[index] == "z":
+        return next_password(password[:index] + "a" + password[index + 1 :], index - 1)
+    return password[:index] + chr(ord(password[index]) + 1) + password[index + 1 :]
+
+
+for _ in range(2):
+    password = next_password(password)
+    while True:
+        if (
+            re.search(
+                r"abc|bcd|cde|def|efg|fgh|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz", password
+            )
+            and not re.search(r"i|l|o", password)
+            and re.search(r"(.)\1.*(.)\2", password)
+        ):
+            break
+        password = next_password(password)
+
+print(password)
