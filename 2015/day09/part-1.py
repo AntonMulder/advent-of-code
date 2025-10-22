44e35e73186e18b6a45821cb36f09d60d871df24
Anton Mulder <anton.mulder@wearespindle.com>
2025-10-22 18:40:52 +0200

    2015 - day 9

---
+++ 2015/day09/part-1.py
@@ -0,0 +1,27 @@
+import re
+from itertools import permutations
+
+from utils.io import read_input_lines
+
+data = read_input_lines()
+
+distances = {}
+locations = set()
+for x in data:
+    start, end, distance = re.match("(.+) to (.+) = (.+)", x).groups()
+    distances[(start, end)] = int(distance)
+    distances[(end, start)] = int(distance)
+    locations.add(start)
+    locations.add(end)
+
+
+def compute_distance(current_location: str, remaining_locations: tuple[str]) -> int:
+    distance = distances[(current_location, remaining_locations[0])]
+    if len(remaining_locations) > 1:
+        return distance + compute_distance(
+            remaining_locations[0], remaining_locations[1:]
+        )
+    return distance
+
+
+print(min(compute_distance(route[0], route[1:]) for route in permutations(locations)))
