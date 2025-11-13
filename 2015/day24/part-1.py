7f4110d6dc73a7a85f7fecebaebd42810a3a30cb
Anton Mulder <anton.mulder@wearespindle.com>
2025-11-13 19:51:58 +0100

    2015 - day 24

---
+++ 2015/day24/part-1.py
@@ -0,0 +1,21 @@
+from functools import reduce
+from itertools import combinations
+from operator import mul
+
+from utils.io import read_input_lines
+
+packages = [int(x) for x in read_input_lines()]
+
+GROUP_WEIGHT = sum(packages) // 3
+
+items = 1
+while not (
+    possible_combinations := [
+        reduce(mul, combination)
+        for combination in combinations(packages, items)
+        if sum(combination) == GROUP_WEIGHT
+    ]
+):
+    items += 1
+
+print(min(possible_combinations))
