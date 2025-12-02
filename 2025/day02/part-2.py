3cdc3f40595e25dccb34d55a233de8ef604bc624
Anton Mulder <anton.mulder@wearespindle.com>
2025-12-02 07:02:55 +0100

    2025 - day 2

---
+++ 2025/day02/part-2.py
@@ -0,0 +1,17 @@
+import re
+
+from utils.io import read_input
+
+product_id_ranges = read_input().split(",")
+
+answer = 0
+for product_id_range in product_id_ranges:
+    start, end = product_id_range.split("-")
+
+    answer += sum(
+        product_id
+        for product_id in range(int(start), int(end) + 1)
+        if re.match(r"^(?!0)(\d*)\1+$", str(product_id))
+    )
+
+print(answer)
