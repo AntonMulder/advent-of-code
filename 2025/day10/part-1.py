07f7562a2a80d66537dfd5c0f06a90ce9fa9501a
Anton Mulder <anton.mulder@wearespindle.com>
2025-12-16 07:02:25 +0100

    Improve day 2025 - day 10, part 1

--- 2025/day10/part-1.py
+++ 2025/day10/part-1.py
@@ -1,36 +1,36 @@
-import copy
-import heapq
 import re
+from collections import deque
 
 from utils.io import read_input_lines as read_data
 
 answer = 0
 for data in read_data():
-    indicator_light, buttons, joltage = re.match(
-        r"^\[(.+)\] (.+) (\{.+\})$", data
-    ).groups()
+    indicator_light, buttons = re.match(r"^\[(.+)\] (.+) .+$", data).groups()
 
-    final_state = [True if light == "#" else False for light in indicator_light]
+    final_state = tuple(light == "#" for light in indicator_light)
+    button_set = [
+        tuple(int(y) for y in re.findall(r"\d", x)) for x in buttons.split(" ")
+    ]
 
-    h = []
-    button_set = [[int(y) for y in re.findall(r"\d", x)] for x in buttons.split(" ")]
-    for buttons in button_set:
-        heapq.heappush(h, (0, [False] * len(indicator_light), buttons))
+    initial_state = tuple(False for _ in indicator_light)
+    queue = deque([(0, initial_state)])
+    visited = {initial_state}
 
-    while True:
-        score, light, buttons = heapq.heappop(h)
-        light = copy.copy(light)
-        score += 1
+    while queue:
+        presses, light = queue.popleft()
 
-        for button in buttons:
-            light[button] = not light[button]
-
-        if final_state == light:
-            answer += score
+        if light == final_state:
+            answer += presses
             break
-        else:
-            for buttons in button_set:
-                if (score, light, buttons) not in h:
-                    heapq.heappush(h, (score, light, buttons))
+
+        for button in button_set:
+            new_light = list(light)
+            for idx in button:
+                new_light[idx] = not new_light[idx]
+            new_light = tuple(new_light)
+
+            if new_light not in visited:
+                visited.add(new_light)
+                queue.append((presses + 1, new_light))
 
 print(answer)
